f231047bc0bcd54581f415b99656f1eac6cfe7ec
Course-666 <cmorse0213@gmail.com>
2026-04-26 16:11:07 -0600

    im not going to make it

--- DAGV1700/AdventureGame/Assets/Scripts/CameraTrigger.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class CameraTrigger : MonoBehaviour
-{
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Something entered: " + other.name);
-    }
-}
